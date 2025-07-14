8b1db15dfa1f39a5b1bcd55d5e9062249b603fa7
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-07-14 13:08:34 -0300

    feat: add-env-

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,9 +1,19 @@
-import { Container } from "./styles";
+import { Container, ContentScrollView, ImagePlant, InfoView, ViewImage } from "./styles";
 
 export default function Dashboard() {
     return (
         <Container>
-            
+            <ContentScrollView
+                showsVerticalScrollIndicator={false}
+            >
+                <ViewImage>
+                    <ImagePlant source={{ uri: 'https://images.tcdn.com.br/img/img_prod/665728/suporte_anel_para_trelica_gancho_vaso_autoirrigavel_15cm_1325_4_b527aeffc8cafdf458dd6b9b25e201a8.jpeg' }} />
+                </ViewImage>
+
+                <InfoView>
+                    <p>Oláaa!!!!</p>
+                </InfoView>
+            </ContentScrollView>
         </Container>
     );
 };
--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,5 +1,24 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
+    flex: 1;
+`;
+
+export const ContentScrollView = styled.ScrollView`
+    margin-top: 5px;
+`;
+
+export const ViewImage = styled.View`
+    width: 100%;
+    height: 190px;
+`;
+
+export const ImagePlant = styled.Image`
+    width: 100%;
+    height: 100%;
+    border-radius: 3px;
+`;
+
+export const InfoView = styled.View`
     
 `;
