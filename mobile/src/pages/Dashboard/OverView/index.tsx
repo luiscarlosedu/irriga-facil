847f3ec754d7dc8e9665a029f0dd9609c6a74dbb
luiscarlosedu <eduardo.luis070408@gmail.com>
2025-08-05 21:37:20 -0300

    feat: last-moistures-crud

--- mobile/src/pages/Dashboard/OverView/index.tsx
+++ mobile/src/pages/Dashboard/OverView/index.tsx
@@ -3,20 +3,21 @@ import { ButtonRow, Container, TitleContainer, Value } from "./styles";
 import { Button, ButtonText, ImagePlant, Label, MoistureCard, MoistureIcon, MoistureInfo, MoistureStatus, MoistureValue, Row, Section, SectionTitle } from "./styles";
 import { useEffect, useState } from "react";
 import { api } from "../../../services/api";
+import { ActivityIndicator, View } from "react-native";
 
 const calcularUmidade = (valor: number): number => {
     return Math.max(0, Math.min(100, 100 - Math.round((valor / 1023) * 100)));
 };
 
 export default function OverView() {
+    const [loading, setLoading] = useState(true);
     const [leituraSensor, setLeituraSensor] = useState<number>(0);
-    const umidade = calcularUmidade(leituraSensor);
-
-    const bombaLigada = false;
     const [ultimaIrrigacao, setUltimaIrrigacao] = useState("Erro ao carregar dados!");
+    const [historicoBruto, setHistoricoBruto] = useState([]);
+    const umidade = calcularUmidade(leituraSensor);
+    const bombaLigada = false;
 
-    const historicoBruto = [920, 780, 650, 1023, 850];
-    const historico = historicoBruto.map(calcularUmidade);
+    const historico = historicoBruto.map(calcularUmidade) ?? "Erro ao carregar dados";
 
     const getStatusUmidade = (): "Alta" | "Moderada" | "Baixa" => {
         if (umidade >= 70) return "Alta";
@@ -27,9 +28,19 @@ export default function OverView() {
     const status = getStatusUmidade();
 
     useEffect(() => {
-        loadLastLog();
-        loadMoisture();
+        loadData();
     }, []);
+
+    async function loadData() {
+        try {
+            setLoading(true);
+            await Promise.all([loadLastLog(), loadMoisture(), loadLastMoitures()]);
+        } catch (err) {
+            console.log("[ERROR]", err);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     async function loadLastLog() {
         try {
@@ -44,11 +55,23 @@ export default function OverView() {
         try {
             const response = await api.get("/moisture");
             setLeituraSensor(response.data.umidade);
-            console.log(response.data.umidade);
         } catch (err) {
             console.log("[ERRO]");
         };
-    }
+    };
+
+    async function loadLastMoitures() {
+        try {
+            const response = await api.get("/last/moistures");
+            const dados = response.data;
+
+            const valores = dados.map((item: { umidade: number }) => item.umidade);
+
+            setHistoricoBruto(valores);
+        } catch (err) {
+            console.log("[ERRO]!");
+        }
+    };
 
     function formatDateTime(isoDate: string) {
         const date = new Date(isoDate);
@@ -62,92 +85,96 @@ export default function OverView() {
     }
 
     return (
-        <Container>
-            {/* 🌿 Planta */}
-            <Section>
-                <SectionTitle>🌿 Planta</SectionTitle>
-                <ImagePlant
-                    source={{
-                        uri: "https://images.tcdn.com.br/img/img_prod/665728/suporte_anel_para_trelica_gancho_vaso_autoirrigavel_15cm_1325_4_b527aeffc8cafdf458dd6b9b25e201a8.jpeg",
-                    }}
-                    resizeMode="cover"
-                />
-                <Label>Monstera Deliciosa</Label>
-            </Section>
+        loading ? (
+            <Container>
+                <ActivityIndicator size="large" color="#4CAF50" style={{ marginTop: 32 }} />
+            </Container>
+        ) : (
+            <Container>
+                {/* 🌿 Planta */}
+                <Section>
+                    <SectionTitle>🌿 Planta</SectionTitle>
+                    <ImagePlant
+                        source={require('../../../assets/monstera.jpeg')}
+                        resizeMode="cover"
+                    />
+                    <Label>Monstera Deliciosa</Label>
+                </Section>
 
-            {/* 💧 Umidade do Solo */}
-            <Section>
-                <TitleContainer>
-                    <Ionicons name="water" size={23} color={"#48C5F3"} />
-                    <SectionTitle>Umidade do Solo</SectionTitle>
-                </TitleContainer>
+                {/* 💧 Umidade do Solo */}
+                <Section>
+                    <TitleContainer>
+                        <Ionicons name="water" size={23} color={"#48C5F3"} />
+                        <SectionTitle>Umidade do Solo</SectionTitle>
+                    </TitleContainer>
 
-                <MoistureCard status={status}>
-                    <MoistureInfo>
-                        <MoistureValue>{umidade}%</MoistureValue>
-                        <MoistureStatus status={status}>{status}</MoistureStatus>
-                    </MoistureInfo>
+                    <MoistureCard status={status}>
+                        <MoistureInfo>
+                            <MoistureValue>{umidade}%</MoistureValue>
+                            <MoistureStatus status={status}>{status}</MoistureStatus>
+                        </MoistureInfo>
 
-                    <MoistureIcon>
-                        <MaterialCommunityIcons
-                            name="water-percent"
-                            size={32}
-                            color={
-                                status === "Alta"
-                                ? "#673AB7" // roxo úmido
-                                : status === "Moderada"
-                                ? "#4CAF50"
-                                : "#FF5722"
-                            }
-                        />
-                    </MoistureIcon>
+                        <MoistureIcon>
+                            <MaterialCommunityIcons
+                                name="water-percent"
+                                size={32}
+                                color={
+                                    status === "Alta"
+                                    ? "#673AB7" // roxo úmido
+                                    : status === "Moderada"
+                                    ? "#4CAF50"
+                                    : "#FF5722"
+                                }
+                            />
+                        </MoistureIcon>
 
-                </MoistureCard>
-            </Section>
+                    </MoistureCard>
+                </Section>
 
-            {/* ⚙️ Bomba */}
-            <Section>
-                <TitleContainer>
-                    <FontAwesome6 name="gear" size={23} color={"#81ACBB"} />
-                    <SectionTitle>Bomba</SectionTitle>
-                </TitleContainer>
-                <Label>Status: {bombaLigada ? "Ligada" : "Desligada"}</Label>
-            </Section>
+                {/* ⚙️ Bomba */}
+                <Section>
+                    <TitleContainer>
+                        <FontAwesome6 name="gear" size={23} color={"#81ACBB"} />
+                        <SectionTitle>Bomba</SectionTitle>
+                    </TitleContainer>
+                    <Label>Status: {bombaLigada ? "Ligada" : "Desligada"}</Label>
+                </Section>
 
-            {/* 🕒 Última Irrigação */}
-            <Section>
-                <TitleContainer>
-                    <Feather name="clock" size={23} />
-                    <SectionTitle>Última Irrigação</SectionTitle>
-                </TitleContainer>
-                <Label>{ultimaIrrigacao}</Label>
-            </Section>
+                {/* 🕒 Última Irrigação */}
+                <Section>
+                    <TitleContainer>
+                        <Feather name="clock" size={23} />
+                        <SectionTitle>Última Irrigação</SectionTitle>
+                    </TitleContainer>
+                    <Label>{ultimaIrrigacao}</Label>
+                </Section>
 
-            {/* 📈 Histórico */}
-            <Section>
-                <TitleContainer>
-                    <Octicons name="graph" size={23} />
-                    <SectionTitle>Histórico de Umidade</SectionTitle>
-                </TitleContainer>
-                {historico.map((item, index) => (
-                    <Row key={index}>
-                        <Label>Registro {index + 1}:</Label>
-                        <Value>{item}%</Value>
-                    </Row>
-                ))}
-            </Section>
+                {/* 📈 Histórico */}
+                <Section>
+                    <TitleContainer>
+                        <Octicons name="graph" size={23} />
+                        <SectionTitle>Histórico de Umidade</SectionTitle>
+                    </TitleContainer>
+                    {historico.map((item, index) => (
+                        <Row key={index}>
+                            <Label>Registro {index + 1}:</Label>
+                            <Value>{item}%</Value>
+                        </Row>
+                    ))}
+                </Section>
 
-            {/* 🔘 Ações */}
-            <ButtonRow>
-                <Button onPress={() => {}}>
-                    <MaterialIcons name="update" size={20} color={"white"} />
-                    <ButtonText>Atualizar</ButtonText>
-                </Button>
-                <Button onPress={() => {}}>
-                    <MaterialIcons name="shower" size={20} color={"white"} />
-                    <ButtonText>Acionar Bomba</ButtonText>
-                </Button>
-            </ButtonRow>
-        </Container>
+                {/* 🔘 Ações */}
+                <ButtonRow>
+                    <Button onPress={() => loadData()}>
+                        <MaterialIcons name="update" size={20} color={"white"} />
+                        <ButtonText>Atualizar</ButtonText>
+                    </Button>
+                    <Button onPress={() => {}}>
+                        <MaterialIcons name="shower" size={20} color={"white"} />
+                        <ButtonText>Acionar Bomba</ButtonText>
+                    </Button>
+                </ButtonRow>
+            </Container>
+        )
     );
 };
